73544589c11cab8653cc3031a4e2db83a4d0fab2
Gabriel <Gabriel8819@hotmail.com>
2022-04-08 21:03:28 -0400

    project template

--- index.js
+++ index.js
@@ -8,33 +8,31 @@ let pos = {
 }
 
 let canvasData;
+let isDrawing = false;
 
 resizeCanvas();
 
 
 
 
-
+//To delete
 ctx.fillStyle = "grey"
 ctx.fillRect(0, 0, canvas.width, canvas.height)
-
+//To delete
 
 
 window.addEventListener("wheel", (e)=>{
     if(e.ctrlKey === true) e.preventDefault()
-    console.log("wheel", e.ctrlKey)
 }, {passive: false})
 
 
 window.addEventListener("resize", (e)=>{
     canvasData = ctx.getImageData(0 , 0, canvas.width, canvas.height);
-
-
     resizeCanvas();
-
+    //To delete
     ctx.fillStyle = "grey";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+    //to delete
     ctx.putImageData(canvasData, 0 , 0);
 })
 
@@ -56,33 +54,33 @@ function resizeCanvas(){
     }
 }
 
-
+canvas.addEventListener("mousemove", canvasMouseDrag)  
 
 function canvasMouseDown(e){
     ctx.beginPath();
-    canvas.addEventListener("mousemove", canvasMouseDrag)  
+    isDrawing = true;
+    // canvas.addEventListener("mousemove", canvasMouseDrag)  
 }
 
 function canvasMouseUp(e){
     ctx.closePath()
-    canvas.removeEventListener("mousemove", canvasMouseDrag)
+    isDrawing = false;
+    // canvas.removeEventListener("mousemove", canvasMouseDrag)
 }
 
 function canvasMouseDrag(e){
+    if(!isDrawing) return;
+
     let scrollX = window.scrollX;
     let scrollY = window.scrollY;
-
     let x = scrollX + e.clientX;
     let y = scrollY + e.clientY;
 
-    console.log(x, y)
-
     ctx.fillStyle = "black";
-   
-    // ctx.arc(pos.x, pos.y, 10, 0, Math.PI*2);
-    ctx.lineWidth = 40
-    ctx.lineTo(x, y)
-    // ctx.fill();
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+    ctx.lineWidth = 40;
+    ctx.lineTo(x, y);
     ctx.stroke();
     
     
